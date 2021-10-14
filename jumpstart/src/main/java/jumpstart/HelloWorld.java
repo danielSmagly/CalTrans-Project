9f9ed7cc417cb6728ef1877583ad126426359248
koral <74123610+koralreeef@users.noreply.github.com>
2021-10-14 13:48:27 -0700

    iText setup
    whole project folder, should be able to work off of this

---
+++ jumpstart/src/main/java/jumpstart/HelloWorld.java
@@ -0,0 +1,38 @@
+package jumpstart;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class HelloWorld 
+{
+	public static final String DEST = "results/chapter01/hello_world.pdf";
+	
+	public static void main(String args[]) throws IOException 
+	{
+		File file = new File(DEST);
+		file.getParentFile().mkdirs();
+		new HelloWorld().createPdf(DEST);
+	}
+	
+	public void createPdf(String dest) throws IOException
+	{
+		FileOutputStream fos = new FileOutputStream(dest);
+		PdfWriter writer = new PdfWriter(fos);
+		
+		PdfDocument pdf = new PdfDocument(writer);
+		
+		Document document = new Document(pdf);
+		
+		document.add(new Paragraph("csc131 team very awesome"));
+		
+		document.add(new Paragraph("csc131 team very awesome 2"));
+		
+		document.close();
+	}
+}
+	
