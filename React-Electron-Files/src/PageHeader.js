b9d31ba6c6d440265e387862ba7a9cbb9e6b2a54
danielSmagly <77174573+danielSmagly@users.noreply.github.com>
2021-11-22 12:51:35 -0800

    Add files via upload
    Completed page header component, includes dynamic page number count.

---
+++ React-Electron-Files/src/PageHeader.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PageHeader = ({pageNumber}) => {
+    return (
+        <>
+        <Styling>
+            <p1 className="title1">STATE OF CALIFORNIA - DEPARTMENT OF TRANSPORTATION</p1>
+            <p1 className="title2">PROJECT CONSTRUCTION STORMWATER REVIEW REPORT</p1>
+            <p1 className="title3 alignleft">ENV-WQP-0003 REV 10/2015</p1>
+            <p1 className="title3 alignright">Page {pageNumber} of 5</p1>
+        </Styling>
+        </>
+    );
+};
+
+const Styling = styled.div`
+    .title1 {
+        display: block;
+        font-size: 0.75rem;
+        line-height: normal;
+    }
+
+    .title2 {
+        display: block;
+        font-size: 1.25rem;
+        line-height: normal;
+        font-weight: bold;
+    }
+
+    .title3 {
+        display: block;
+        font-size: 0.75rem;
+        line-height: normal;
+    }
+
+    .alignleft {
+        float: left;
+    }
+    
+    .alignright {
+        float: right;
+    }
+`;
+
+export default PageHeader;
