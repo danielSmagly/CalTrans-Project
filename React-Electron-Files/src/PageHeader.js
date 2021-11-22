98348791b772a78f84b74cb75b59193ef58292d3
danielSmagly <77174573+danielSmagly@users.noreply.github.com>
2021-11-22 15:23:46 -0800

    Update PageHeader.js
    Added Line break to PageHeader component.

--- React-Electron-Files/src/PageHeader.js
+++ React-Electron-Files/src/PageHeader.js
@@ -8,7 +8,8 @@ const PageHeader = ({pageNumber}) => {
             <p1 className="title1">STATE OF CALIFORNIA - DEPARTMENT OF TRANSPORTATION</p1>
             <p1 className="title2">PROJECT CONSTRUCTION STORMWATER REVIEW REPORT</p1>
             <p1 className="title3 alignleft">ENV-WQP-0003 REV 10/2015</p1>
-            <p1 className="title3 alignright">Page {pageNumber} of 5</p1>
+            <p1 className="title3 alignright">Page {pageNumber} of 5</p1><br/>
+            <hr class="solid"></hr>
         </Styling>
         </>
     );
@@ -40,6 +41,10 @@ const Styling = styled.div`
     
     .alignright {
         float: right;
+    }
+    .solid {
+        border-top: 2px solid #000000;
+        display: block;
     }
 `;
 
