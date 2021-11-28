86d01f7480addbcabf02f0e7dac880416fffab73
danielSmagly <77174573+danielSmagly@users.noreply.github.com>
2021-11-27 16:04:13 -0800

    Update App.js

--- React-Electron-Files/src/App.js
+++ React-Electron-Files/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import PageHeader from './PageHeader';
+import ProjectDescription from './ProjectDescription';
+import DateAndVerification from './DateAndVerification';
+import CssBaseline from '@mui/material/CssBaseline';
+import styled from 'styled-components';
+import DateAdapter from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
-function App() {
+
+function App({children}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <CssBaseline/>
+        <PageSize>
+          <Formater>
+          <PageHeader pageNumber={1}/>
+          <br/>
+          <ProjectDescription/>
+          <br/><br/>
+          </Formater>
+          <Formater>
+          <LocalizationProvider dateAdapter={DateAdapter}>{children}</LocalizationProvider>
+          <DateAndVerification/>
+          </Formater>
+        </PageSize>
+    </>
   );
 }
+
+const PageSize = styled.div`
+
+text-align: left;
+margin: 25px 100px ;
+padding: 0px;
+`;
+
+const Formater = styled.div`
+  display: block;
+`;
 
 export default App;
