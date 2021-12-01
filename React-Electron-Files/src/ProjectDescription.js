8a77341fb3383875a1106f23866aa041e60c92b0
danielSmagly <77174573+danielSmagly@users.noreply.github.com>
2021-12-01 14:38:10 -0800

    Added ability to chose District number.

--- React-Electron-Files/src/ProjectDescription.js
+++ React-Electron-Files/src/ProjectDescription.js
@@ -2,6 +2,10 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const Item1 = () => {
   return (
@@ -19,6 +23,13 @@ const Item1 = () => {
 };
 
 const Item2 = () => {
+
+  const [district, setDistrict] = React.useState('');
+
+  const handleChange = (event) => {
+    setDistrict(event.target.value);
+  };
+
   return (
     <>
       <Box sx={{ ml: 1 }}>
@@ -35,14 +46,29 @@ const Item2 = () => {
           </Grid>
 
           <Grid item xs={2}>
-            <TextField style={{ minWidth: '7rem' }}
-              id="outlined-multiline-static"
-              label="DISTRICT"
-              multiline
-              rows={1}
-              placeholder="District"
-              fullWidth
-            />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label" fontSize="1">DISTRICT</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={district}
+                label="DISTRICT"
+                onChange={handleChange}
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={6}>6</MenuItem>
+                <MenuItem value={7}>7</MenuItem>
+                <MenuItem value={8}>8</MenuItem>
+                <MenuItem value={9}>9</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={11}>11</MenuItem>
+                <MenuItem value={12}>12</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
 
           <Grid item xs={3}>
@@ -88,3 +114,4 @@ export default function ProjectDescription() {
     </Box>
   );
 };
+
