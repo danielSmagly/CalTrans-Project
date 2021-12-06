1025cf32981e817952920e16a6528a652db91b4d
lsamoy989 <92546769+lsamoy989@users.noreply.github.com>
2021-12-05 17:48:41 -0800

    Add files via upload
    -Creates the button that adds another admin review

---
+++ React-Electron-Files/src/NewAdminReview.js
@@ -0,0 +1,26 @@
+import React, { Suspense } from 'react'
+import Button from '@mui/material/Button';
+import loadable from '@loadable/component'
+
+const AdminReviewLoad = loadable(() => import('./AdminReview'))
+
+export default function NewAR() {
+
+    const [loadAR, setAR] = React.useState(() => {
+        return 0
+    })
+    
+    return (
+        <div>
+            <Button variant="outlined" onClick={() => setAR(prevAR => prevAR + 1)}>
+                ADD ADMINISTRATIVE FINDING
+            </Button>
+
+            {loadAR ? (
+                <Suspense fallback={<div>Loading Component...</div>}>
+                    <AdminReviewLoad />
+                </Suspense>
+            ) : null}
+        </div>
+    )
+}
