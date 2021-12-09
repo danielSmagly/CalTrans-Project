2405ef72a686415528b3c3de716f843408b57b50
danielSmagly <77174573+danielSmagly@users.noreply.github.com>
2021-12-08 16:39:01 -0800

    Update RegulatoryStatus.js

--- React-Electron-Files/src/RegulatoryStatus.js
+++ React-Electron-Files/src/RegulatoryStatus.js
@@ -9,6 +9,41 @@ import Chip from '@mui/material/Chip';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import { useTheme } from '@mui/material/styles';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+];
+
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 const Item1 = () => {
   return (
@@ -19,6 +54,19 @@ const Item1 = () => {
 }
 
 const Item2 = () => {
+
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a the stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
   return(
     <>
       <Grid container spacing ={1}>
@@ -55,16 +103,35 @@ const Item2 = () => {
         
         <Grid item xs={11}>
           <Box sx={{minWidth: 100}}>
-            <FormControl fullWidth sx={{mt: 0.6}} >
-              <TextField
-              id = "outlined-mutiline-static"
-              label = "PLACS (PERMITS,LICENSES,AGREEMENTS,CERTIFICATIONS)SPECIFYING TEMPORARY BMP REQIREMENTS"
-              multiline
-              rows={1}
-              placeholder="BODIES OF WATER HERE"
-              fullWidth
-              />
-            </FormControl>
+          <FormControl sx={{ m: 1, width: 300 }}>
+        <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
+        <Select
+          labelId="demo-multiple-chip-label"
+          id="demo-multiple-chip"
+          multiple
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+          renderValue={(selected) => (
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+              {selected.map((value) => (
+                <Chip key={value} label={value} />
+              ))}
+            </Box>
+          )}
+          MenuProps={MenuProps}
+        >
+          {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, personName, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
           </Box>
         </Grid>
       </Grid>
