2e7896f84ac027cdfc2d4acd5fdb4a40a4dc1a22
lsamoy989 <92546769+lsamoy989@users.noreply.github.com>
2021-12-08 19:02:50 -0800

    Update AdminReview.js
    Updated to auto-populate Standard/Standard Ref (Note: You will probably have to press the enter key into the text fields when they auto-populate)

--- React-Electron-Files/src/AdminReview.js
+++ React-Electron-Files/src/AdminReview.js
@@ -23,37 +23,76 @@ const Item1 = () => {
     const handleChangeQ = (event) => {
         setQuestion(event.target.value);
     }
+
     var QList1, QList2, QList3, QList4 = null;
     if (category.includes("Plans and Permits")) {
-        QList1 = "A1 - Is the signed SWPPP available at the construction site?"
-        QList2 = "A2 - Is the SWPPP (and any amendments) prepared and signed by a certified QSD?"
+        QList1 = "A1 - Is an updated SWPPP available at the construction site?"
+        QList2 = "A2 - Was the SWPPP and any amendments prepared and signed by a certified QSD?"
         QList3 = "None"
         QList4 = "None"
     } else if (category.includes("Training")) {
-        QList1 = "B1 - Does the WPC manager have a current certification (QSD) in the project files?"
-        QList2 = "B2 - Is the contractor conducting the required stormwater training and maintaining adequate records of this training in the project files?"
+        QList1 = "B1 - Do the WPCM and any dedicated QSPs and QSDs have current certifications?"
+        QList2 = "B2 - Is the contractor conducting the required stormwater training and maintaining adequate records of this training?"
         QList3 = "None"
         QList4 = "None"
     } else if (category.includes("SMARTS")) {
-        QList1 = "C1 - Has the Storm Water Annual Report been developed and submitted (in the project files) for the last reporting year as required?"
+        QList1 = "C1 - Has the most recently required Stormwater Annual Report been properly certified and uploaded into SMARTS?"
         QList2 = "None"
         QList3 = "None"
         QList4 = "None"
     } else if (category.includes("Active Treatment Systems")) {
-        QList1 = "D1 - For projects that use an ATS, has the ATS been designed and is operated by qualified personnel, and do reporting requirements appear to be up to date and documented in the project files?"
+        QList1 = "D1 - For projects that use an ATS, does the ATS operation meet all the requirements of the CGP, Attachment F?"
         QList2 = "None"
         QList3 = "None"
         QList4 = "None"
     } else if (category.includes("Monitoring and Reporting")) {
         QList1 = "E1 - Are inspections, monitoring, and reporting procedures performed as required documented in the project files?"
-        QList2 = "E2 - If a NAL is exceeded, has a construction site and run-on evaluation been prepared and documented in the project files?"
-        QList3 = "E3 - Is there a QSP Communication/Management Plan included in the SWPPP or on site?"
-        QList4 = "E4 - Is there a dewatering/diversion plan included as part of the SWPPP if required?"
-    } else if (category.includes("Tahoe CGP-Specific Requirements")) {
-        QList1 = "None"
-        QList2 = "None"
-        QList3 = "None"
+        QList2 = "E2 - If a NAL/NEL is exceeded, has a construction site and run-on evaluation been prepared and documented in the project files?"
+        QList3 = "E3 - Have NAL Exceedance Reports been submitted to the SWRCB within 10 days of the conclusion of storm events?"
         QList4 = "None"
+    } else if (category.includes("Tahoe CGP-Specific Requirements")) {
+        QList1 = "F1 - For projects subject to the Taho CGP, for Restoration projects, is there a detailed Restoration Monitoring Plan as part of the CSMRP?"
+        QList2 = "F2 - For projects subject to the Taho CGP, If the project has activities within 100 year floodplains and stream environmental zones (SEZ) within the Lake Tahoe basin; are there waste discharge prohibition exemptions on file?"
+        QList3 = "F3 - For projects subject to the Taho CGP, have all turbidity and pH analytical results collected from field instruments been reported in SMARTS within 5 days after the conclusion of a storm event?"
+        QList4 = "None"
+    }
+
+    var autoStandRef = null;
+    if (category.includes("Plans and Permits") && question === 10){
+        autoStandRef = "SPECs, 13-1.03A General"
+    } else if (category.includes("Plans and Permits") && question === 20){
+        autoStandRef = "SPECs, 13-3.01A Summary"
+    } else if (category.includes("Training") && question === 10){
+        autoStandRef = "SPECs, 13-1.01D(3)(a) General"
+    } else if (category.includes("Training") && question === 20){
+        autoStandRef = "SPECs, 13-1.01D(2) Training"
+    } else if (category.includes("SMARTS") && question === 10){
+        autoStandRef = "SPECs, 13-3.01B(8) Storm Water Annual Report"
+    } else if (category.includes("Active Treatment Systems") && question === 10){
+        autoStandRef = "SPECs, 13-8.01A Summary"
+    } else if (category.includes("Monitoring and Reporting") && question === 10){
+        autoStandRef = "SPECs, 13-3.01B(4) Site Inspection Reports"
+    } else if (category.includes("Monitoring and Reporting") && question === 20){
+        autoStandRef = "SPECs, 13-3.01B(6)(b) Numeric Action Level Exceedance Report"
+    }
+
+    var autoStand = null;
+    if (category.includes("Plans and Permits") && question === 10){
+        autoStand = "Retain a printed copy of the authorized WPCP or SWPPP at the job site."
+    } else if (category.includes("Plans and Permits") && question === 20){
+        autoStand = "You may assign a QSD other than the WPC manager to develop the SWPPP"
+    } else if (category.includes("Training") && question === 10){
+        autoStand = "The WPC manager must be a QSP if the project requires a WPCP. The WPC manager must be a QSD if the project requires a SWPPP."
+    } else if (category.includes("Training") && question === 20){
+        autoStand = "2. Conduct weekly training meetings covering: 2.1 Deficiencies and corrective actions for water pollution control practices; 2.2 water pollution control practices required for work activities during the week; 2.3 Spill prevention and control; 2.4 Material delivery, storage, usage, and disposal; 2.5 Waste management; 2.6 Non-stormwater management procedures"
+    } else if (category.includes("SMARTS") && question === 10){
+        autoStand = "Submit the storm water annual report before July 15th if construction occurs from July 1st through June 30th or within 15 days after the Contract acceptance if construction ends before June 30th"
+    } else if (category.includes("Active Treatment Systems") && question === 10){
+        autoStand = "Design, installation, operation, and monitoring of an ATS and monitoring of the treated effluent must comply with Attachment F of NPDES General Permit for Stormwater Discharges Associated with Construction and Land Disturbance Activities (Order No. 2009-0009-DWQ, NPDES no. CAS000002)."
+    } else if (category.includes("Monitoring and Reporting") && question === 10){
+        autoStand = "The WPC manager must submit the following informational submittals within 24 hours of completing a weekly inspection: 1. Completed Stormwater Site Inspection Report form."
+    } else if (category.includes("Monitoring and Reporting") && question === 20){
+        autoStand = "Whenever a NAL is exceeded, notify the Engineer and submit a NAL exceedance report within 48 hours after the conclusion of a storm event."
     }
 
     return(
@@ -138,6 +177,7 @@ const Item1 = () => {
                     multiline
                     rows={1}
                     placeholder="Enter Standard Reference"
+                    value = {autoStandRef}
                 />
             </Box>
 
@@ -149,6 +189,7 @@ const Item1 = () => {
                     multiline
                     rows={2}
                     placeholder="Enter Standard"
+                    value = {autoStand}
                 />
             </Box>
             <br/>
@@ -210,3 +251,4 @@ const CAStyle = styled.div`
     font-size: 1.10rem;
     font-weight: 500;
 `;
+
