ea7635084ef67b64e6fa257128593f61abdd6fc7
tjaramillo452329 <92546766+tjaramillo452329@users.noreply.github.com>
2021-12-08 18:11:42 -0800

    Add files via upload

--- React-Electron-Files/src/RegulatoryStatus.js
+++ React-Electron-Files/src/RegulatoryStatus.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+//import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import FormControl from '@mui/material/FormControl';
 import styled from 'styled-components';
@@ -23,16 +23,36 @@ const MenuProps = {
 };
 
 const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
+  'None',
+  'U.S. Army Corps of Engineer - Federal CWA Section 404 Dredge and Fill Permit',
+  'U.S. Fish and Wildlife Service - Federal ESA Section 7 - Biological Opinion for flora and fauna',
+  'U.S. Nation Marine Fisheries Service - Federal ESA Section 7 - Biological Opnion for fisheries',
+  'California Department of Fish and Wildlife - California ESA Inccidental Take Permit',
+  'California RWQCB - Federal CWA Section 401 Water Quality Certification',
+  'Califonia Department of Fish and Wildlife Service - California FI..E Code Section 1602 Lake or Stream Bed Alteration Agreement',
+  'California Coastal Commission - Coastal Development Permit',
+  'San Fransisco Bay Conservation and Development Commission - San Fransisco Bay Conservation and Development Permit',
+  'Californa Resources Agency - Flood Protection Board Encroachment Permit',
+  'Califonia SWRCB - Statewide General WDRs for Discharges to Land with a Low Threat to Water Quality',
+  'California RWQCB - Waste Discharge Requirements for Dewatering and other Low Threat Discharges to Surface Waters',
+  'California State Lands Commision - PRC 1766.9 Public Agency Lease Agreement',
+  'Other (Indicate other Stormwater related PLACS)',
+];
+
+const rwqcb = [
+  'North Coast - Region 1',
+  'San Francisco - Region 2',
+  'Central Coast - Region 3',
+  'Los Angeles - Region 4',
+  'Central Valley - Region 5 Sacramento',
+  'Central Valley - Region 5 Fresno',
+  'Central Valley - Region 5 Redding',
+  'Lahontan - Region 6 - South Lake Tahoe',
+  'Lahontan - Region 6 - Victorville',
+  'Colorado River - Region 7',
+  'Santa Ana - Region 8',
+  'San Diego - Region 9',
+
 ];
 
 
@@ -54,6 +74,11 @@ const Item1 = () => {
 }
 
 const Item2 = () => {
+  const [age, setAge] = React.useState('');
+
+    const changeHandle = (event) => {
+        setAge(event.target.value);
+    };
 
   const theme = useTheme();
   const [personName, setPersonName] = React.useState([]);
@@ -65,46 +90,72 @@ const Item2 = () => {
     setPersonName(
       // On autofill we get a the stringified value.
       typeof value === 'string' ? value.split(',') : value,
+      
     );
   };
   return(
     <>
       <Grid container spacing ={1}>
-        <Grid item xs={3}>
+      <Grid item xs={2}>
           <Box sx={{minWidth: 100}}>
             <FormControl fullWidth >
-              <TextField
-              id = "outlined-mutiline-static"
-              label = "SWPPP OR WPCP"
-              multiline
-              rows={1}
-              placeholder="SWPPP OR WPCP HERE"
-              fullWidth
-              />
+              <InputLabel id="demo-simple-select-label" fontSize="1">SWPPP OR WPCP</InputLabel>
+              <Select
+                labeId="demo-simple-select-label"
+                id="demo-simple-select"
+                value = {age}
+                label= "SWPPP OR WPCP"
+                onChange={changeHandle}
+                >
+                <MenuItem value={1}>None</MenuItem>
+                <MenuItem value={2}>SWPPP</MenuItem>
+                <MenuItem value={3}>WPCP</MenuItem>
+                </Select>
+
             </FormControl>
           </Box>
         </Grid>
 
-        <Grid item xs={8}>
+        <Grid item xs={14}>
           <Box sx={{minWidth:100}}>
             <FormControl fullWidth >
-              <TextField
-              id = "outlined-mutiline-static"
-              label = "RWQCB(S)"
-              multiline
-              rows={1}
-              placeholder="RWQCB(S) HERE"
-              fullWidth
-              />
+              <InputLabel id="demo-multiple-chip-label">RWQCB(S)</InputLabel>
+              <Select 
+              labelId="demo-multiple-chip-label"
+              id="demo-multiple-chip"
+              multiple
+              value={personName}
+              onChange={handleChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {rwqcb.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                  style={getStyles(name, personName, theme)}
+                >
+                  {name}
+                </MenuItem>
+              ))}
+            </Select>
+
             </FormControl>
           </Box>
         </Grid>
 
         
-        <Grid item xs={11}>
+        <Grid item xs={16}>
           <Box sx={{minWidth: 100}}>
-          <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-chip-label">Chip</InputLabel>
+          <FormControl fullWidth>
+        <InputLabel id="demo-multiple-chip-label">PLACS</InputLabel>
         <Select
           labelId="demo-multiple-chip-label"
           id="demo-multiple-chip"
@@ -142,11 +193,11 @@ const Item2 = () => {
 export default function RegulatoryStatus() {
   return(
     <Box sx={{flexGrow: 1}}>
-      <Grid container spacing={2} columns={11}>
-        <Grid item xs={12}>
+      <Grid container spacing={2} columns={16}>
+        <Grid item xs={16}>
           <Item1/>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={16}>
           <Item2/>
         </Grid>
       </Grid>
